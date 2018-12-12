2af270436d1066da7739bddce0ff1b8fdc74b4ab
DakwonSmith <dsmith@nyc.yearup.org>
2018-12-12 16:50:31 -0500

    The ruby bakery

---
+++ server.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+require 'cookies.rb'
+require 'cakes.rb'
+require 'muffins.rb'
+
+get '/' do
+    erb :index
+end
+
+get '/cookies' do
+
+end
+
+get '/cakes' do
+
+end
+
+get '/muffins' do
+
+end
