79ac1f04134bb5cf9654c6d6b485186c17f7fa8d
DakwonSmith <dsmith@nyc.yearup.org>
2018-12-19 14:39:46 -0500

    Fully baked

--- server.rb
+++ server.rb
@@ -1,4 +1,7 @@
 require 'sinatra'
+require './muffins'
+require './cakes'
+require './cookies'
 
 get '/' do
   erb :index
@@ -13,5 +16,8 @@ get '/cakes' do
 end
 
 get '/muffins' do
+  @@chocoMuffins = Muffins.new('Chocolate Meltdown',3.29,'A delicious chocolatey heaven that you have never entered. Once you are in, you will never want to leave')
+  @@choasMuffins = Muffins.new('Choas Control',9.29,'This is honestly a mess, I mean a real mess. I hope you can control it')
+  @@rainbowMuffins = Muffins.new('Unicornicopia',8.29,'With the power of princes Celestia, You will be able to share the power of friendship.')
   erb :muffins
 end
